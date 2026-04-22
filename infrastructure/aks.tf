d8669422ede2855300ec64245d6525b0a5b98710
Mohomed Althaf <malthaf55@gmail.com>
2026-04-23 03:15:47 +0400

    updated azure cluster to use amd64

--- infrastructure/aks.tf
+++ infrastructure/aks.tf
@@ -4,10 +4,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "devaks"
 
+  sku_tier = "Free"
+
   default_node_pool {
     name       = "system"
-    node_count = 2
-    vm_size    = "Standard_D2ps_v6"
+    vm_size    = "Standard_D2s_v3"   # AMD64 ONLY
+    node_count = 1
+
+    os_sku = "Ubuntu"
+
+    type = "VirtualMachineScaleSets"
   }
 
   identity {
@@ -15,6 +21,23 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   network_profile {
-    network_plugin = "azure"
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "amd64" {
+  name                  = "amd64pool"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+
+  vm_size    = "Standard_D2s_v3"   # AMD64 ONLY
+  node_count = 1
+
+  mode    = "User"
+  os_type = "Linux"
+  os_sku  = "Ubuntu"
+
+  node_labels = {
+    "kubernetes.io/arch" = "amd64"
   }
 }
