4b3507af070d0032afbef0623634c8bcffd1af91
Mohomed Althaf <malthaf55@gmail.com>
2026-04-23 19:24:12 +0400

    updated text in app

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1>E& Enterprise Devops Demo - Mohomed Althaf</h1>;
+	return <h1>e& Enterprise Devops Demo - Mohamed Althaf new update</h1>;
 }
